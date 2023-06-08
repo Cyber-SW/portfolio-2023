1fa8348d633a4a09f67f3cdef93e3154ac176836
shawn <woltersh@outlook.de>
2023-06-08 19:37:03 +0200

    reworking navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 function Navbar() {
   const [toggleNav, setToggleNav] = useState(false);
@@ -13,8 +15,34 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-container">
+        <div className={`nav-links  ${toggleNav && "active"}`}>
+          <ul>
+            <li>
+              <NavLink to="About" className="nav-align">ABOUT</NavLink>
+            </li>
+            <li>
+              <NavLink to="Work" className="nav-align">WORK</NavLink>
+            </li>
+            <li>
+              <NavLink to="Contact" className="nav-align">CONTACT</NavLink>
+            </li>
+          </ul>
+        </div>
         <div className="nav-logo">
           <img src={logo} alt="logo-sw" />
+        </div>
+        <div className={`nav-links  ${toggleNav && "active"}`}>
+          <ul>
+            <li>
+              <NavLink to="GitHub" className="github-link"><FontAwesomeIcon icon={faGithub} size="2x" /></NavLink>
+            </li>
+            <li>
+              <NavLink to="LinkedIn"><FontAwesomeIcon icon={faLinkedin} size="2x" /></NavLink>
+            </li>
+            <li>
+              <NavLink to="CodeWars" className={`codewars-icon  ${toggleNav && "active"}`}></NavLink>
+            </li>
+          </ul>
         </div>
         <div
           className={`menu-icon ${toggleNav && "active"}`}
@@ -23,19 +51,6 @@ function Navbar() {
           <span className="bar"></span>
           <span className={`bar middle ${toggleNav && "active"}`}></span>
           <span className="bar"></span>
-        </div>
-        <div className={`nav-links  ${toggleNav && "active"}`}>
-          <ul>
-            <li>
-              <NavLink to="About">ABOUT</NavLink>
-            </li>
-            <li>
-              <NavLink to="Work">WORK</NavLink>
-            </li>
-            <li>
-              <NavLink to="Contact">CONTACT</NavLink>
-            </li>
-          </ul>
         </div>
       </div>
     </nav>
